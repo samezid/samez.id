ffb903dc9214251ba1d0a482606732866bd2f8c7
Fachrizal Ainurrahman <fachrizalainurrahman@gmail.com>
2024-06-08 19:19:29 +0700

    first

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/style.css">
+
+    <title>Welcome to Samezid</title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <div class="nav">
+        <h1>samezid</h1>
+        <div class="nav-icon">
+            <i>instagram</i>
+            <i>github</i>
+            <i>gmail</i>
+        </div>
+    </div>
+
+    <!--hero -->
+    <main>
+        <div class="greeting">
+            <h1 class="hxl">Nice to meet you! <br> i'm Fachrizal Ainurrahman</h1>
+            <p>With extensive programming experience since age 9, I've cultivated a fervent passion for machine learning. Committed to continuous learning, I aspire to leverage my competencies in this field to eventually build my career.</p>
+
+            <a href="#">CONTACT ME</a>
+        </div>
+        <div class="picture">
+            <img src="#" alt="">
+        </div>
+
+        <!-- skill -->
+        <div>
+            <h1>Javascript</h1>
+            <p>50+ Hours Experience</p>
+        </div>
+        <div>
+            <h1>PHP</h1>
+            <p>150+ Hours Experience</p>
+        </div>
+        <div>
+            <h1>Design Graphic</h1>
+            <p>5 Years Experience</p>
+        </div>
+
+        <!-- portfolio -->
+        <div>
+            <h1>Project</h1>
+            <a>CONTACT ME</a>
+        </div>
+
+        <div>
+            <img src="#" alt="">
+            <h3>Design Portfolio</h3>
+        </div>
+
+        <div>
+            <img src="#" alt="">
+            <h3>Company Profile</h3>
+        </div>
+
+        <div>
+            <img src="#" alt="">
+            <h3>News Website</h3>
+        </div>
+    </main>
+
+    <!-- Contact -->
+    <div>
+        <div>
+            <h1>Contact</h1>
+            <p>I would love to hear about your project and how I can help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
+        </div>
+        <div>
+            <form method="post">
+                <input type="text" placeholder="Name">
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Subject">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                <input type="submit" value="SEND">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
